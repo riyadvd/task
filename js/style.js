ef0465c97b135f43d12528753f02809c7b2b6957
riyadvd <riyadwvd3@gmail.com>
2020-12-03 01:24:32 +0530

    all files

---
+++ js/style.js
@@ -0,0 +1,9 @@
+$("#nav-btn").click(function(){
+    $("#sidenavmenu").toggleClass("show");
+    $("#nav-overlay").toggleClass("overlay-add");
+});
+
+$("#nav-btn2").click(function(){
+    $("#sidenavmenu").toggleClass("show");
+    $("#nav-overlay").toggleClass("overlay-add");
+});
